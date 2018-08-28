ca3e7826812157ba0cd8a0ec46d8eec17f384834
Konstantin Chupin <kchupin41@gmail.com>
2018-08-28 19:09:31 +0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,45 @@
+import os
+import setuptools
+
+
+class CleanCommand(setuptools.Command):
+    """Custom clean command to tidy up the project root."""
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    # noinspection PyMethodMayBeStatic
+    def run(self):
+        os.system('rm -vrf ./build ./dist ./*.pyc ./*.tgz ./*.egg-info')
+
+
+setuptools.setup(
+    name="fspy",
+
+    python_requires="==3.6.*",
+
+    use_scm_version=True,
+
+    author="Konstantin Chupin",
+    author_email="kchupin41@gmail.com",
+    description="File System Spy",
+
+    setup_requires=[
+        "setuptools_scm"
+    ],
+
+    install_requires=[
+    ],
+
+    packages=setuptools.find_packages(),
+
+    classifiers=['Private :: Do Not Upload'],
+
+    cmdclass={
+        'clean': CleanCommand,
+    }
+)
