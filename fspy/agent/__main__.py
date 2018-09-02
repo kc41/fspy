d5a8a6e1b0cc5a08e3b6c9b2f5768013776fc90d
Konstantin Chupin <kchupin41@gmail.com>
2018-09-02 20:59:10 +0700

    - Async agent runner (WS sending not ready)

--- fspy/agent/__main__.py
+++ fspy/agent/__main__.py
@@ -1,4 +1,25 @@
+from os import path
 from fspy.agent import runner
+from fspy.common import defaults
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser("FSPY client")
+    parser.add_argument("--target", type=str, help="Directory to scan", required=True)
+    parser.add_argument("--server_host", type=str, help="FSPY server hostname/IP", default="127.0.0.1")
+    parser.add_argument("--server_port", type=int, help="FSPY server port", default=defaults.DEFAULT_PORT)
+
+    args = parser.parse_args()
+
+    ws_url = f"ws://{args.server_host}:{args.server_port}/ws"
+    target = path.abspath(args.target)
+
+    if not path.isdir(target):
+        print(f"{target} is not a directory")
+
+    runner.main(ws_url=ws_url, scan_target=target)
+
 
 if __name__ == '__main__':
-    runner.main()
+    main()
--- fspy/agent/runner.py
+++ fspy/agent/runner.py
@@ -1,4 +1,7 @@
+from typing import Optional
+
 import asyncio
+import aiohttp
 
 import logging
 import logging.config
@@ -20,29 +23,105 @@ def log_full_diff(full_diff: FullDiff):
             log.debug("File was updated: %s", fds)
 
 
-async def main_coro(scanner: SimpleComparator):
-    while True:
+class Agent:
+    def __init__(self, ws_url: str, scan_target: str, loop: asyncio.AbstractEventLoop):
+        self._loop = loop
+        self._scanner = SimpleComparator(scan_target)
+
+        self._ws_url = ws_url
+        self._ws = None
+        self._session = aiohttp.ClientSession(loop=loop)
+
+        self._diff_send_task = None  # type: asyncio.Task
+        self._diff_queue = asyncio.Queue(loop=loop)
+
+        self._launch_delay = 2
+
+    async def _get_ws(self) -> Optional[aiohttp.ClientWebSocketResponse]:
+        if self._ws is None:
+            log.info("Connecting to web-socket")
+            # noinspection PyBroadException
+            try:
+                self._ws = await self._session.ws_connect(self._ws_url)
+            except Exception:
+                log.exception("Failed to connect to web-socket")
+                return None
+
+        return self._ws
+
+    async def _send_diff_attempt(self, diff) -> bool:
+        ws = await self._get_ws()
+
+        if ws is None:
+            log.warning("Can not get WS. Waiting for next attempt")
+            return False
+
+        # TODO FIX: send diff
+        return True
+
+    async def _send_diff(self, diff, max_attempts=3, attempts_delay=5) -> bool:
+
+        for attempt_num in range(0, max_attempts):
+            success = await self._send_diff_attempt(diff)
+            if success:
+                return True
+
+            await asyncio.sleep(attempts_delay)
+
+        return False
+
+    async def _serve_diff_queue(self):
+        while True:
+            log.debug("Awaiting next diff in queue")
+            next_diff = await self._diff_queue.get()
+
+            log.debug("Diff received from queue")
+            await self._send_diff(next_diff)
+
+    # TODO FIX: use single thread executor to totally prevent parallel launches of SimpleComparator
+    async def _collect_diff(self):
         # noinspection PyBroadException
         try:
             log.info("Running directory scan")
-            full_diff = scanner.scan()
+            full_diff = await self._loop.run_in_executor(None, self._scanner.scan)
             log.info("Directory scan finished")
 
             if full_diff:
-                log.info(f"FS changes detected " 
+                log.info(f"FS changes detected "
                          f"created: {len(full_diff.created)} "
                          f"deleted: {len(full_diff.deleted)} "
                          f"updated: {len(full_diff.updated)}")
                 log_full_diff(full_diff)
+
+                await self._diff_queue.put(full_diff)
             else:
                 log.info("No FS changes was detected")
+
         except Exception:
-            log.exception("Exception in main cycle")
+            log.exception("Exception during diff collection")
 
-        await asyncio.sleep(2)
+    async def close(self):
+        if self._ws:
+            await self._ws.close()
+
+        if self._diff_send_task:
+            self._diff_send_task.cancel()
+
+        await self._session.close()
+
+    async def run(self):
+        # Initial attempt to connect
+        await self._get_ws()
+        log.info("Running diff send task")
+        self._diff_send_task = asyncio.ensure_future(self._serve_diff_queue(), loop=self._loop)
+
+        log.info("Starting infinite directory scanning")
+        while True:
+            await self._collect_diff()
+            await asyncio.sleep(self._launch_delay)
 
 
-def main():
+def main(scan_target: str, ws_url: str):
     logging.config.dictConfig({
         'version': 1,
         'disable_existing_loggers': False,
@@ -73,12 +152,18 @@ def main():
     loop = asyncio.get_event_loop()
 
     log.info("Client app start")
+
+    agent = Agent(
+        ws_url=ws_url,
+        scan_target=scan_target,
+        loop=loop
+    )
+
     try:
-        loop.run_until_complete(main_coro(
-            scanner=SimpleComparator(".")
-        ))
+        loop.run_until_complete(agent.run())
     except KeyboardInterrupt:
         pass
     finally:
         log.info("Client app stop")
+        loop.run_until_complete(agent.close())
         loop.close()
